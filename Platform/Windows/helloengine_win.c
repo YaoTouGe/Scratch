1172bd8babe7ab8d51baccc28d6aaaf08b1700e5
Wang Yd <450468524@qq.com>
2021-10-05 23:07:09 +0800

    finish d2d

--- Platform/Windows/helloengine_win.c
+++ Platform/Windows/helloengine_win.c
@@ -1,54 +1,72 @@
 #include <tchar.h>
 #include <windows.h>
 #include <windowsx.h>
-
+#include <wingdi.h>
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam,
                             LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-                   LPTSTR lpCmdLine, int nCmdShow) {
-  HWND hWnd;
-  WNDCLASSEX wc;
+                   LPTSTR lpCmdLine, int nCmdShow)
+{
+    HWND hWnd;
+    WNDCLASSEX wc;
 
-  ZeroMemory(&wc, sizeof(WNDCLASSEX));
-  wc.cbSize = sizeof(WNDCLASSEX);
-  wc.style = CS_HREDRAW | CS_VREDRAW;
-  wc.lpfnWndProc = WindowProc;
-  wc.hInstance = hInstance;
-  wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-  wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
-  wc.lpszClassName = _T("WindowClass1");
+    ZeroMemory(&wc, sizeof(WNDCLASSEX));
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.lpszClassName = _T("WindowClass1");
 
-  RegisterClassEx(&wc);
+    RegisterClassEx(&wc);
 
-  hWnd = CreateWindowEx(0, _T("WindowClass1"), _T("Hello, Engine!"),
-                        WS_OVERLAPPEDWINDOW, 300, 300, 500, 400, NULL, NULL,
-                        hInstance, NULL);
+    hWnd = CreateWindowEx(0, _T("WindowClass1"), _T("Hello, Engine!"),
+                          WS_OVERLAPPEDWINDOW, 300, 300, 500, 400, NULL, NULL,
+                          hInstance, NULL);
 
-  ShowWindow(hWnd, nCmdShow);
+    ShowWindow(hWnd, nCmdShow);
 
-  MSG msg;
+    MSG msg;
 
-  while (GetMessage(&msg, NULL, 0, 0)) {
-    TranslateMessage(&msg);
-    DispatchMessage(&msg);
-  }
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
 
-  return msg.wParam;
+    return msg.wParam;
 }
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam,
-                            LPARAM lParam) {
-  switch (message) {
-  case WM_DESTROY: {
-    PostQuitMessage(0);
-    return 0;
-  } break;
+                            LPARAM lParam)
+{
+    switch (message)
+    {
 
-  default:
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        RECT rec = {20, 20, 60, 80};
+        HBRUSH brush = (HBRUSH)GetStockObject(BLACK_BRUSH);
+
+        FillRect(hdc, &rec, brush);
+        EndPaint(hWnd, &ps);
+    }
     break;
-  }
+    case WM_DESTROY:
+    {
+        PostQuitMessage(0);
+        return 0;
+    }
+    break;
 
-  return DefWindowProc(hWnd, message, wParam, lParam);
+    default:
+        break;
+    }
+
+    return DefWindowProc(hWnd, message, wParam, lParam);
 }
